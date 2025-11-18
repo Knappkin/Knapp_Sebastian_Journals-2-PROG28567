2eed7dc55e2290484b30623f8e20f49a8fd869e9
Knappkin <44062671+Knappkin@users.noreply.github.com>
2025-11-18 12:34:25 -0500

    Week 11 In-class
    Added basic jump using apex time and height as controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField] private float maxSpeed;
-    [SerializeField] private float acceltime;
-    [SerializeField] private float deceltime;
+    //Rigidbody
+    Rigidbody2D rb;
+    public float gravity;
+    ////Horizontal Movement
+    //[Header("Running Variables")]
+    //[SerializeField] private float maxSpeed;
+    //[SerializeField] private float acceltime;
+    //[SerializeField] private float deceltime;
+    [SerializeField] private float moveSpeed;
+
+    //Jumping
+    [Header("Jump Variables")]
+    [SerializeField] private float apexTime;
+    [SerializeField] private float apexHeight;
+    private float initialJumpVelo;
+    private bool useJump;
+    private bool isJumping;
+    private float jumpStartTime;
+    private float currentJumpTime;
+
+    [SerializeField] Vector2 playerInput = new Vector2();
     public enum FacingDirection
     {
         left, right
@@ -16,17 +35,54 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        gravity = -2 * apexHeight / (apexTime * apexTime);
+        initialJumpVelo = 2 * apexHeight / apexTime;
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+      
         playerInput.x = Input.GetAxisRaw("Horizontal");
         MovementUpdate(playerInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            useJump = true;
+            isJumping = true;
+            jumpStartTime = Time.deltaTime;
+            currentJumpTime = jumpStartTime;
+        }
+
+        if (isJumping)
+        {
+            currentJumpTime += Time.deltaTime;
+            //Debug.Log(rb.linearVelocityY);
+        }
+
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        rb.linearVelocityY += gravity * Time.fixedDeltaTime;
+
+        rb.linearVelocityX = moveSpeed * playerInput.x;
+        if (useJump)
+        {
+            //useJump = false;
+
+
+            Debug.Log(currentJumpTime);
+            rb.linearVelocityY = gravity * currentJumpTime + initialJumpVelo;
+        }
     }
 
     private void MovementUpdate(Vector2 playerInput)
