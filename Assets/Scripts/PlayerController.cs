4cffa80d85ab7db180434fd6cbc327a3b2aa27b7
Knappkin <44062671+Knappkin@users.noreply.github.com>
2025-11-20 01:09:26 -0500

    Worked on week 10 journal
    Trying to catch up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,12 +9,18 @@ public class PlayerController : MonoBehaviour
     //Rigidbody
     Rigidbody2D rb;
     public float gravity;
-    ////Horizontal Movement
-    //[Header("Running Variables")]
-    //[SerializeField] private float maxSpeed;
-    //[SerializeField] private float acceltime;
-    //[SerializeField] private float deceltime;
+
+    //Horizontal Movement
+    [Header("Running Variables")]
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float accelTime;
+    [SerializeField] private float decelTime;
     [SerializeField] private float moveSpeed;
+
+    private float lastFacingDir;
+
+    private float acceleration;
+    private float deceleration;
 
     //Jumping
     [Header("Jump Variables")]
@@ -25,6 +31,8 @@ public class PlayerController : MonoBehaviour
     private bool isJumping;
     private float jumpStartTime;
     private float currentJumpTime;
+
+    [SerializeField] public LayerMask groundMask;
 
     [SerializeField] Vector2 playerInput = new Vector2();
     public enum FacingDirection
@@ -43,13 +51,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        acceleration = maxSpeed / accelTime;
+        deceleration = maxSpeed / decelTime;
         gravity = -2 * apexHeight / (apexTime * apexTime);
         initialJumpVelo = 2 * apexHeight / apexTime;
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
       
         playerInput.x = Input.GetAxisRaw("Horizontal");
-        MovementUpdate(playerInput);
+      
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -65,6 +75,10 @@ public class PlayerController : MonoBehaviour
             //Debug.Log(rb.linearVelocityY);
         }
 
+        IsWalking();
+
+       
+        Debug.Log(IsGrounded());
         
 
     }
@@ -72,35 +86,90 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
 
-        rb.linearVelocityY += gravity * Time.fixedDeltaTime;
+       // rb.linearVelocityY += gravity * Time.fixedDeltaTime;
 
-        rb.linearVelocityX = moveSpeed * playerInput.x;
+        //rb.linearVelocityX = moveSpeed * playerInput.x;
+        MovementUpdate(playerInput);
+
+
         if (useJump)
         {
             //useJump = false;
 
 
-            Debug.Log(currentJumpTime);
+            //Debug.Log(currentJumpTime);
             rb.linearVelocityY = gravity * currentJumpTime + initialJumpVelo;
         }
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        if (playerInput.x != 0)
+        {
+            
+       
+        if (Mathf.Abs(rb.linearVelocityX) < maxSpeed)
+        {
+            rb.linearVelocityX += playerInput.x * acceleration * Time.deltaTime;
+            //Debug.Log(rb.linearVelocityX);
+        }
+            else
+            {
+                rb.linearVelocityX = maxSpeed * playerInput.x;
+            }
+        }
 
+        else {
+            rb.linearVelocityX += playerInput.x * deceleration * Time.deltaTime;
+        }
     }
 
     public bool IsWalking()
     {
-        return false;
+        if (rb.linearVelocityX != 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+       
     }
     public bool IsGrounded()
     {
-        return true;
+        if (Physics2D.BoxCast(rb.position, Vector2.one, 0f, Vector2.down, .2f, groundMask))
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+
     }
 
     public FacingDirection GetFacingDirection()
     {
-        return FacingDirection.left;
+
+        if (playerInput.x < 0)
+        {
+            lastFacingDir = 0;
+        }
+        if (playerInput.x > 0) 
+        {
+            
+            lastFacingDir = 1;
+        }
+
+        if(lastFacingDir == 0)
+        {
+            return FacingDirection.left;
+        }
+        else
+        {
+            return FacingDirection.right;
+        }
     }
 }
