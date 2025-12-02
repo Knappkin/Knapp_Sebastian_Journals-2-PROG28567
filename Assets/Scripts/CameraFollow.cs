fe15e9c7a79daae4d821b228b8112525d8150c9e
Knappkin <44062671+Knappkin@users.noreply.github.com>
2025-12-02 11:23:53 -0500

    Basic camera follow
    camera follows player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform targetHolder;
+    public float followIntensity;
+
+    private float cameraZDepth;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cameraZDepth = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = targetHolder.position;
+        targetPosition.z = cameraZDepth;
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followIntensity * Time.deltaTime);
+
+    }
+}
